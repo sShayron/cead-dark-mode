fbafd7e5c5e688cf7ef98f1c55ed4d6a5147ea6c
Shayron Aguiar <shayron.aguiar@gmail.com>
2021-02-17 12:10:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.body.style.backgroundColor = '#000';
 var style = document.createElement('style');
 style.type = 'text/css';
-style.innerHTML = 'section p, section strong { color: #fff!important }';
+style.innerHTML = 'section p, section strong, section em { color: #fff!important } #menu_flutuante { background-color: #3d3d3d }';
 document.getElementsByTagName('head')[0].appendChild(style);
