e7bbfe0d720d06659fd96256852d20796ff7ccd4
Shayron Aguiar <shayron.aguiar@gmail.com>
2021-02-27 18:52:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-document.body.style.backgroundColor = '#000';
 var style = document.createElement('style');
 style.type = 'text/css';
 style.innerHTML = `
+body {
+  background-color: #000;
+}
+#page-content .card-body, #page-content .userprofile .profile_tree section, .userprofile .profile_tree #page-content section, .card {
+  border: none;
+  background-color: #000;
+  color: #fff;
+}
 section p, section strong, section em {
   color: #fff!important
 }
-#menu_flutuante {
+#menu_flutuante, nav.navbar {
   background-color: #3d3d3d
 } 
 ::-moz-selection {
